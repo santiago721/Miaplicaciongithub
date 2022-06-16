06deb6295f24dcd7566951bc7e9cc76fea56375d
pluto <staborda539@misena.edu.co>
2022-06-16 11:17:51 -0500

    mostrando algo

--- app/src/main/java/com/example/miaplicaciongithub/MainActivity.java
+++ app/src/main/java/com/example/miaplicaciongithub/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.miaplicaciongithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "monstrando algo", Toast.LENGTH_SHORT).show();
     }
 }
